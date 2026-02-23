8fdb335759a20ce81f154918769f76941b4db295
muaatu <muaatu368@gmail.com>
2026-02-23 16:06:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ threading.Thread(target=run_server, daemon=True).start()
 
 def send_telegram_message(message):
     token = "7649557476:AAH66AAnP-pXl0_SsqTAn0uP896z6N5_x8E"
-    chat_id = "7052737604"
+    chat_id = "7808566419"
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {"chat_id": chat_id, "text": message}
     try:
